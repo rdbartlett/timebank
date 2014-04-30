194df8e6bc7d4ed74afbe98101039d5c5527419b
Richard Bartlett <rich@loomio.org>
2014-04-30 17:53:48 +1200

    whitespace fixup

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,9 @@ class Contact < ActiveRecord::Base
     Contact.delete_all
     CSV.foreach(file.path, headers: true) do |row|
       imported_row = row.to_hash.extract!("First Name", "Surname", "Preferred contact")
+      imported_row["First Name"].strip! unless imported_row["First Name"].nil?
+      imported_row["Surname"].strip! unless imported_row["Surname"].nil?
+      imported_row["Preferred contact"].strip! unless imported_row["Preferred contact"].nil?
       merged = {"name" => "#{imported_row["First Name"]} #{imported_row["Surname"]}", "details" => "#{imported_row["Preferred contact"]}"}
       Contact.create!(merged)
     end
